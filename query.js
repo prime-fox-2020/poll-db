5bf9a473203167bf6bc7bd7359de9d4fb2ba869f
elvanrafif <terlanjurbuta@gmail.com>
2020-04-12 01:00:20 +0700

    release 2 console.table

--- query.js
+++ query.js
@@ -10,59 +10,59 @@ const client = new Client({
 
 
 client.connect()
-//RELEASE 2 QUERY PERTAMA
-// client.query(`
-// SELECT DISTINCT politician.name,politician.party,politician.location,politician.grade_current
-// FROM politician
-// WHERE party = 'R'
-// AND grade_current BETWEEN 9 AND 11
-// ORDER BY grade_current ASC`, (err, res) =>{
-//   if (err) console.log(err);
-//   console.log(res.rows);
-//   // client.end()
-// })
+// RELEASE 2 QUERY PERTAMA
+client.query(`
+SELECT DISTINCT politician.name,politician.party,politician.location,politician.grade_current
+FROM politician
+WHERE party = 'R'
+AND grade_current BETWEEN 9 AND 11
+ORDER BY grade_current ASC`, (err, res) =>{
+  if (err) console.log(err);
+  console.table(res.rows);
+  // client.end()
+})
 
 //RELEASE 2 QUERY KEDUA
-// client.query(`
-//   SELECT COUNT(voters_id) AS total_vote, politician.name
-//   FROM votes JOIN politician
-//   ON votes.pejabat_id = politician.id
-//   WHERE politician.name = 'Olympia Snowe'
-//   GROUP BY politician.name
-//   `, (err, res) =>{
-//     if (err) console.log(err);
-//     console.log(res.rows);
-//     // client.end()
-//   })
+client.query(`
+  SELECT COUNT(voters_id) AS total_vote, politician.name
+  FROM votes JOIN politician
+  ON votes.pejabat_id = politician.id
+  WHERE politician.name = 'Olympia Snowe'
+  GROUP BY politician.name
+  `, (err, res) =>{
+    if (err) console.log(err);
+    console.table(res.rows);
+    // client.end()
+  })
 
 //RELEASE 2 QUERY KETIGA 
-  // client.query(`
-  //   SELECT politician.name, COUNT(voters_id) AS total_vote
-  //   FROM votes JOIN politician
-  //   ON votes.pejabat_id = politician.id
-  //   WHERE politician.name LIKE 'Adam%'
-  //   GROUP BY politician.name
-  //   ORDER BY politician.name
-  //   `, (err, res) =>{
-  //     if (err) console.log(err);
-  //     console.log(res.rows);
-  //     // client.end()
-  //   })
+  client.query(`
+    SELECT politician.name, COUNT(voters_id) AS total_vote
+    FROM votes JOIN politician
+    ON votes.pejabat_id = politician.id
+    WHERE politician.name LIKE 'Adam%'
+    GROUP BY politician.name
+    ORDER BY politician.name
+    `, (err, res) =>{
+      if (err) console.log(err);
+      console.table(res.rows);
+      // client.end()
+    })
 
   
 //RELEASE 2 QUERY KEEMPAT
-  // client.query(`
-  //   SELECT COUNT(voters_id) AS total_vote, politician.name, politician.party, politician.location
-  //   FROM votes JOIN politician
-  //   ON votes.pejabat_id = politician.id
-  //   GROUP BY politician.name, politician.party, politician.location
-  //   ORDER BY COUNT (*) DESC
-  //   LIMIT 3
-  //   `, (err,res)=>{
-  //   if (err) console.log(err)
-  //   console.log(res.rows);
-  //   // client.end()
-  // })
+  client.query(`
+    SELECT COUNT(voters_id) AS total_vote, politician.name, politician.party, politician.location
+    FROM votes JOIN politician
+    ON votes.pejabat_id = politician.id
+    GROUP BY politician.name, politician.party, politician.location
+    ORDER BY COUNT (*) DESC
+    LIMIT 3
+    `, (err,res)=>{
+    if (err) console.log(err)
+    console.table(res.rows);
+    // client.end()
+  })
 
 //RELEASE 2 QUERY KELIMA
   client.query(`
@@ -76,6 +76,6 @@ client.connect()
       ORDER BY voters.first_name ASC)
       `, (err,res) =>{
         if(err) console.log(err)
-        console.log(res.rows);
+        console.table(res.rows);
         client.end()
       })

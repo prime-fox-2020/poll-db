74222bed9ec49cb08d8d935fa938e2cbc667b5f6
fahrulnoer <fahrulnoer17@gmail.com>
2020-04-11 13:43:55 +0700

    poll-db

---
+++ seed-data.js
@@ -0,0 +1,72 @@
+const fs = require('fs')
+const client = require('./connection')
+
+class Seeding {
+    static seedPoll(){
+        let politicians = fs.readFileSync('./politicians.csv', 'utf8').split('\n')
+        const tampungPoll = []
+        for (let i = 0; i < politicians.length; i++) {
+            tampungPoll.push(politicians[i].split(','))
+        }
+
+        for (let i = 0; i < tampungPoll.length; i++) {
+            let queryPoll = `INSERT INTO politicians(name, partai, location, grade_current)
+                             VALUES ($1, $2, $3, $4)`
+
+            client.query(queryPoll, tampungPoll[i], (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(tampungPoll[i]);
+                }
+            })
+        }
+    }
+
+    static seedVoters(){
+        let voters = fs.readFileSync('./voters.csv', 'utf8').split('\n')
+        const tampungVoters = []
+        for (let i = 0; i < voters.length; i++) {
+            tampungVoters.push(voters[i].split(','))
+        }
+
+        for (let i = 0; i < tampungVoters.length; i++) {
+            let queryVoters = `INSERT INTO voters(first_name, last_name, gender, age)
+                               VALUES ($1, $2, $3, $4)`
+            
+            client.query(queryVoters, tampungVoters[i], (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(tampungVoters[i]);
+                }
+            })
+        }
+    }
+
+    static seedVotes(){
+        let votes = fs.readFileSync('./votes.csv', 'utf8').split('\n')
+        const tampungVotes = []
+        for (let i = 0; i < votes.length; i++) {
+            tampungVotes.push(votes[i].split(','))
+        }
+
+        for (let i = 0; i < tampungVotes.length; i++) {
+            let queryVotes = `INSERT INTO votes(voter_id, politician_id)
+                              VALUES ($1, $2)`
+            
+            client.query(queryVotes, tampungVotes[i], (err, data) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(tampungVotes[i]);
+                }
+            })
+        }
+
+    }
+}
+
+Seeding.seedPoll()
+Seeding.seedVoters()
+Seeding.seedVotes()
--- setup.js
+++ setup.js
@@ -1,1 +1,48 @@
 //your code here
+const client = require('./connection')
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS politicians (
+        id_politicians serial PRIMARY KEY,
+        name VARCHAR (50),
+        partai VARCHAR (50), 
+        location VARCHAR (50),
+        grade_current FLOAT
+    )
+` , (err, res) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(res)
+    }
+})
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS voters (
+        id serial PRIMARY KEY,
+        first_name VARCHAR (50),
+        last_name VARCHAR (50),
+        gender VARCHAR (50),
+        age INTEGER
+    )
+` , (err, res) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(res)
+    }
+})
+
+client.query(`
+    CREATE TABLE IF NOT EXISTS votes (
+        id serial PRIMARY KEY,
+        voter_id INTEGER,
+        politician_id INTEGER
+    )
+` , (err, res) => {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log(res)
+    }
+})
